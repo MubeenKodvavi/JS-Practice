c09877ac09c88cc29c7835b013dbbb86d25b2c75
Mubeen Kodvavi <mubeen.kodvavi@gmail.com>
2020-06-07 01:32:06 +0500

    chapter1

---
+++ app.js
@@ -0,0 +1,19 @@
+//Chapter 1:
+//1
+alert("Hello visitor");
+
+//2
+alert("Error! Please enter a valid password");
+
+//3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+//4
+alert("Welcome to JS");
+alert("Happy Coding!");
+
+//5
+//SS attached
+
+//Chapter 2:
+//1
