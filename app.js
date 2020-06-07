61a90d1fd7d16a3e23c76afc37c4b6920c81f673
Mubeen Kodvavi <mubeen.kodvavi@gmail.com>
2020-06-07 12:24:40 +0500

    chapter 3/4

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ alert("Happy Coding!");
 //5
 //SS attached
 
+
 //Chapter 2:
 //1
 var username;
@@ -40,7 +41,7 @@ var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
 alert(pizza);
 
 //6
-email = "mubeen.kodvavi@gmail.com";
+var email = "mubeen.kodvavi@gmail.com";
 alert("My email address is " + email);
 
 //7
@@ -54,4 +55,51 @@ document.write("Yah! I can write HTML content through JavaScript");
 var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
 alert(design);
 
-//
+
+//Chapter 3:
+//1
+var age = 22;
+alert("I am " + age + " years old");
+
+//2
+var n = 22;
+alert("You have visited the site " + n + " times");
+
+//3
+var birthYear = 1998;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is " + typeof(birthYear) + "<br>");
+
+//4
+var visitorName = "John Doe";
+var productTitle = "T-Shirt";
+var quantity = 5;
+document.write("<strong>" + visitorName + "</strong> ordered <strong>" + quantity + " " + productTitle + "</strong>(s) on XYZ Clothing Store");
+
+//Chapter 4
+//1
+var var1, var2, var3;
+
+//2
+//Legal
+var name;
+var birthYear;
+var $name;
+var my_name;
+var student321;
+
+//Illegal
+// var my name;
+// var if;
+// var 321 student;
+// var !name;
+// var for;
+
+//3
+document.write("<h1>" + "Rules for naming JS variables" + "</h1>");
+document.write("Variable names can only contain numbers, $ and _. For example: $my_1stVariable." + "<br>");
+document.write("Variable must begin with a letter, $ or _. For example: $name, _name or name." + "<br>");
+document.write(" Variable names are case sensitive." + "<br>");
+document.write("Variable names should not be JS keywords." + "<br>");
+
+//Chapter 5:
