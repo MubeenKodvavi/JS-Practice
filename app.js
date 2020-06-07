cab978036f00e9cd155bf6e5c7144a2341c5cdcb
Mubeen Kodvavi <mubeen.kodvavi@gmail.com>
2020-06-07 11:39:51 +0500

    chapter 2

--- app.js
+++ app.js
@@ -17,3 +17,41 @@ alert("Happy Coding!");
 
 //Chapter 2:
 //1
+var username;
+
+//2
+var myName = "Mubeen Kodvavi";
+
+//3
+var message;
+message = "Hello World";
+alert(message);
+
+//4
+var name = "Jhone doe";
+var age = 15;
+var profession = "Certified Mobile Application Development";
+alert(name);
+alert(15 + " years old");
+alert(profession);
+
+//5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+//6
+email = "mubeen.kodvavi@gmail.com";
+alert("My email address is " + email);
+
+//7
+var book = "A smarter way to learn JavaScript.";
+alert("I am trying to learn from the Book " + book);
+
+//8
+document.write("Yah! I can write HTML content through JavaScript");
+
+//9
+var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+
+//
