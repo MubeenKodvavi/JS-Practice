edf5a61a3bf0ac27dc7c6151d14f1f51fc5bd266
Mubeen Kodvavi <mubeen.kodvavi@gmail.com>
2020-06-27 02:27:59 +0500

    html

---
+++ Chapter 21 - 38/app.js
@@ -0,0 +1,11 @@
+//Chapter 21 to 25
+//1
+var firstName = prompt("Enter first name");
+var lastName = prompt("Enter last name");
+var fullName = firstName + lastName;
+alert("Hello " + fullName);
+
+//2
+var favPhone = prompt("Enter your favourite phone model:");
+document.write("My favourite phone is " + favPhone + "<br>");
+document.write("Length of string: " + favPhone.length + "<br>");
